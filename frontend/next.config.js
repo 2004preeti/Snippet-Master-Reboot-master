5546efc09650e4232a18c8b8409dbb9373f0f15c
preeti pal <preeti2072004@gmail.com>
2025-05-10 20:01:37 +0530

    frist-commit

---
+++ frontend/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        fs: false,
+        path: false,
+        os: false,
+      },
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+// module.exports = {
+//   basePath: '/Reboot-master',
+//   assetPrefix: '/Reboot-master',
+// };
